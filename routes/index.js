10b07b5d37929abcda5193bb64e5b28b55048b97
NizarDawood <nizar.dawood@elev.ga.nrig.se>
2023-05-26 08:37:44 +0200

    cart problem

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const session = require('express-session');
 const router = express.Router();
 const { response } = require('express');
 const bcrypt = require('bcrypt');
 const pool = require('../utils/database.js');
 const promisePool = pool.promise();
 
+// Configure session middleware
+router.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
 
 router.get('/', async function (req, res, next) {
     const [rows] = await promisePool.query("SELECT * FROM edzarshop");
@@ -14,126 +21,102 @@ router.get('/', async function (req, res, next) {
     });
 });
 
-
 router.get('/login', async function (req, res, next) {
     res.render('login.njk', { title: 'Log' });
 });
 
 router.get('/shop', async function (req, res, next) {
-
     const [rows] = await promisePool.query("SELECT * FROM edzarshop");
     res.render('shop.njk', { title: 'PostIt', name: req.session.username, rows: rows });
-
-
 });
 
-
-
 router.post('/shop', async function (req, res, next) {
-    req.body = { logout };
-
-
+    // req.body = { logout };
 });
 
 router.get('/logout', async function (req, res, next) {
-
     res.render('logout.njk', { title: 'Logout' });
     req.session.login = 0;
 });
 
 router.post('/shop', async function (req, res, next) {
     req.body = { profile };
-
-
 });
 
 router.get('/profile', async function (req, res, next) {
-
-    res.render('profile.njk', { title: 'profile' });
-    req.session.login = 0;
+    if (req.session.login == 1) {
+        res.render('profile.njk', { title: 'Profile', name: req.session.username });
+    } else {
+        return res.status(401).send('Access denied');
+    }
 });
-
-
 
 router.post('/login', async function (req, res, next) {
     const { username, password } = req.body;
 
-
     if (username.length == 0) {
-        return res.send('Username is Required')
+        return res.send('Username is Required');
     }
     if (password.length == 0) {
-        return res.send('Password is Required')
+        return res.send('Password is Required');
     }
 
     const [user] = await promisePool.query('SELECT * FROM edzarusers WHERE name = ?', [username]);
 
-
     bcrypt.compare(password, user[0].password, function (err, result) {
-
         if (result === true) {
             req.session.username = username;
             req.session.login = 1;
             return res.redirect('/shop');
+        } else {
+            return res.send("Invalid username or password");
         }
-
-        else {
-            return res.send("Invalid username or password")
-        }
-
-    })
-
-
+    });
 });
 
 router.get('/crypt/:password', async function (req, res, next) {
-    const password = req.params.password
+    const password = req.params.password;
     bcrypt.hash(password, 10, function (err, hash) {
         return res.json({ hash });
-
-    })
+    });
 });
-
-
 
 router.get('/register', function (req, res, next) {
     res.render('register.njk', { title: 'register' });
-
 });
 
 router.post('/register', async function (req, res, next) {
     const { username, password, passwordConfirmation } = req.body;
 
     if (username === "") {
-        console.log({ username })
-        return res.send('Username is Required')
-
-    }
-    else if (password.length === 0) {
-        return res.send('Password is Required')
-    }
-    else if (password && password.length <= 8) {
+        console.log({ username });
+        return res.send('Username is Required');
+    } else if (password.length === 0) {
+        return res.send('Password is Required');
+    } else if (password && password.length <= 8) {
         response.errors.push('password must be at least 8 characters');
-    }
-    else if (passwordConfirmation.length === 0) {
-        return res.send('Password is Required')
-    }
-    else if (password !== passwordConfirmation) {
-        return res.send('Passwords do not match')
+    } else if (passwordConfirmation.length === 0) {
+        return res.send('Password is Required');
+    } else if (password !== passwordConfirmation) {
+        return res.send('Passwords do not match');
     }
 
     const [user] = await promisePool.query('SELECT name FROM edzarusers WHERE name = ?', [username]);
-    console.log({ user })
+    console.log({ user });
 
     if (user.length > 0) {
-        return res.send('Username is already taken')
+        return res.send('Username is already taken');
     } else {
         bcrypt.hash(password, 10, async function (err, hash) {
-            const [creatUser] = await promisePool.query('INSERT INTO edzarusers (name, password) VALUES (?, ?)', [username, hash]);
-            res.redirect('/login')
-        })
+            const [createUser] = await promisePool.query('INSERT INTO edzarusers (name, password) VALUES (?, ?)', [username, hash]);
+            req.session.username = createUser.insertId;
+            const [updateUser] = await promisePool.query('UPDATE edzarusers SET name = ? WHERE id = ?', [username, createUser.insertId]);
+            res.redirect('/login');
+        });
     }
 });
+
+module.exports = router;
 
 
 
@@ -179,21 +162,111 @@ router.post('/delete', async function (req, res, next) {
     });
 });
 
+
+
+
+
+
+////////////// hämta produkter
+
+
+  
+  router.get('/profile', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM edzaruser");
+    res.render('profile.njk', { title: 'postIt', name: req.session.username, rows: rows });
+  });
+
+/// skicka data från tabellen edzaruser name och edzarshop id 1 column 
+
+router.get('/shop', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM edzarshop");
+    res.render('shop.njk', { title: 'PostIt', name: req.session.username, rows: rows });
+  });
+  router.get('/shop', async function (req, res, next) {
+    res.render('shop.njk', {
+        title: 'Nytt inlägg',
+    });
+});
+router.post('/add-to-cart', async function (req, res, next) {
+    const { Id } = req.body;
+  
+    // logged in session funkar
+    if (req.session.login === 1 && req.session.username) {
+      const userId = req.session.username;
+      const query = 'INSERT INTO edzarcart (userid, productname, price) VALUES (?, ?, ?)';
+  
+      try {
+
+        const [productRows] = await promisePool.query('SELECT * FROM edzarshop WHERE Id = ?', [Id]);
+        const { productname, price } = productRows[0];
+  
+        // Insert the product into the edzarcart table
+        await promisePool.query(query, [userId, productname, price]);
+        res.redirect('/cart');
+      } catch (error) {
+        console.error('Error inserting product into the cart:', error);
+        return res.sendStatus(500);
+      }
+    } else {
+      return res.status(401).send('Access denied');
+    }
+  });
+  
+
+////////////////FORUM
 router.get('/new', async function (req, res, next) {
     res.render('new.njk', {
         title: 'Nytt inlägg',
     });
 });
-
-
-
 router.post('/new', async function (req, res, next) {
-    const { price, title, link } = req.body;
+    const { author, title, content } = req.body;
     const error = [];
-            const [rows] = await promisePool.query("INSERT INTO edzarshop (price, title, link) VALUES ( ?, ?, ?)", [price, title, link]);
+
+    if (!title) {
+        error.push('Title is required');
+    }
+    if (!content) {
+        error.push('Body is required');
+    }
+    if (title && title.length <= 3) {
+        error.push('Title must be at least 3 characters');
+    }
+    if (content && content.length <= 10) {
+        error.push('Body must be at least 10 characters');
+    }
+    if (res.length === 0) {
+        const sanitize = (str) => {
+            let temp = str.trim();
+            temp = validator.stripLow(temp);
+            temp = validator.escape(temp);
+            return temp;
+        };
+    }
+    if (!title) {
+        sanitizedTitle = sanitize(title)
+    }
+    if (!content) {
+        sanitizedcontent = sanitize(content)
+    }
+    if (error.length === 0) {
+        try {
+            const [rows] = await promisePool.query("INSERT INTO nd20forum (author, title, content) VALUES ( ?, ?, ?)", [author, title, content]);
             res.redirect('/forum');
-        
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        res.send(error);
+    }
+
+
 });
+
+
+  
+  
+
 
 
 
